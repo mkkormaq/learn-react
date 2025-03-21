66a51226a236af1beb90ee78f12c67094ee998f6
Josh McCormack <joshgmc@gmail.com>
2025-03-20 21:04:15 -0400

    set up for challenge 3

--- 02. Data-Driven React/02. Travel Journal - Header/components/Header.jsx
+++ 02. Data-Driven React/02. Travel Journal - Header/components/Header.jsx
@@ -2,7 +2,11 @@ export default function Header() {
   return (
     <header>
       <nav>
-        <img className="header-image" src="globe.png" alt="Globe image" />
+        <img
+          className="header-image"
+          src="images/globe.png"
+          alt="Globe image"
+        />
         <span className="header-text">my travel journal.</span>
       </nav>
     </header>
